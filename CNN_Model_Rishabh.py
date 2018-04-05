f0e65b611d6404d4bb1732ce9b33a03f1e55e478
rishabhsharma22 <rishabh230795@gmail.com>
2018-04-04 17:50:25 -0700

    CNN_Model and weights stored

--- CNN_Model_Rishabh.py
+++ CNN_Model_Rishabh.py
@@ -24,7 +24,8 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
-
+import h5py
+import keras
 # Initialising the CNN
 classifier = Sequential()
 
@@ -58,30 +59,24 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('C:/Deep Learning/Udemy/Deeplearning_a-z/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Deeplearning_a-z/Convolutional_Neural_Networks/dataset/training_set',
+training_set = train_datagen.flow_from_directory('dataset/Training_Data',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('C:/Deep Learning/Udemy/Deeplearning_a-z/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Deeplearning_a-z/Convolutional_Neural_Networks/dataset/test_set',
+test_set = test_datagen.flow_from_directory('dataset/Test_Data',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
+                         steps_per_epoch = 1000,
+                         epochs = 5,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 250)
 
 
-import numpy as np
-from keras.preprocessing import image
-test_image = image.load_img('C:/Deep Learning/Udemy/Deeplearning_a-z/Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/Deeplearning_a-z/Convolutional_Neural_Networks/dataset/single_prediction/cat_or_dog_1.jpg',target_size = (64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-training_set.class_indices
-
+classifier.save("Rishabh_model_1.h5",overwrite=True)
+classifier.summary()
 
 
