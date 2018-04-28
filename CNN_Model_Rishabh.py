6adc28071490930a53d4bd95a00577628f9f0817
Rishabh Sharma <rishabh230795@gmail.com>
2018-04-28 16:11:42 -0700

    Update CNN_Model_Rishabh.py

--- CNN_Model_Rishabh.py
+++ CNN_Model_Rishabh.py
@@ -59,12 +59,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('dataset\Training_Data',
+training_set = train_datagen.flow_from_directory('dataset/Training_Data',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('dataset\Test_Data',
+test_set = test_datagen.flow_from_directory('dataset/Test_Data',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
@@ -78,4 +78,3 @@ history = classifier.fit_generator(training_set,
 
 #classifier.save("Rishabh_model_1.h5",overwrite=True)
 #classifier.summary()
-
